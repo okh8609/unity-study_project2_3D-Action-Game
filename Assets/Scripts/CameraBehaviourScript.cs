d9cafad5874575761e092650dc36eda4a41a90b1
Khaos <okh8609@gmail.com>
2021-08-17 23:25:41 +0800

    Camera 平滑運鏡 (線性內插)

---
+++ Assets/Scripts/CameraBehaviourScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviourScript : MonoBehaviour
+{
+    public Transform cam_pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position = cam_pos.position;
+        this.transform.rotation = cam_pos.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float tt = Time.deltaTime * 10.0f;
+        //print(tt);
+        this.transform.position = Vector3.Lerp(this.transform.position, cam_pos.position, tt);
+        this.transform.rotation = Quaternion.Lerp(this.transform.rotation, cam_pos.rotation, tt);
+    }
+}
