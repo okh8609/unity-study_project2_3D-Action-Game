34dff7c33f75754bf713b6bee1cc56774da7e073
Khaos <okh8609@gmail.com>
2021-08-17 22:58:06 +0800

    加入 [玩家]的 Animation Control：向前走

---
+++ Assets/Scripts/PlayerBehaviourScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviourScript : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dy = Input.GetAxis("Vertical");
+        animator.SetFloat("Speed", dy);
+    }
+}
