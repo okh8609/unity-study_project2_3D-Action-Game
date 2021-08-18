ba2816dd1101f6825019217d255df77ec666817d
Khaos <okh8609@gmail.com>
2021-08-18 21:07:39 +0800

    前進.向左.向右  使用  動畫混合樹 (Blend Tree)

--- Assets/Scripts/PlayerBehaviourScript.cs
+++ Assets/Scripts/PlayerBehaviourScript.cs
@@ -20,7 +20,8 @@ public class PlayerBehaviourScript : MonoBehaviour
         float dy = Input.GetAxis("Vertical");
         animator.SetFloat("Speed", dy);
         float dx = Input.GetAxis("Horizontal");
-        animator.SetFloat("Direction", dx);
+        animator.SetFloat("Direction", dx, 0.08f, Time.deltaTime);
+        //animator.SetFloat("Direction", dx);
 
         //this.transform.Rotate(new Vector3(0, dx * Time.deltaTime * 10.0f, 0), Space.Self);
         //cc.Move(this.transform.forward * dy * Time.deltaTime * 5.0f);
