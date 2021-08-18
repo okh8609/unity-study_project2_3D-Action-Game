8dfa01380b41fd1dbd59e4241f4b6e6e8b1c16c3
Khaos <okh8609@gmail.com>
2021-08-18 15:04:41 +0800

    (基本) 左右轉

--- Assets/Scripts/PlayerBehaviourScript.cs
+++ Assets/Scripts/PlayerBehaviourScript.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerBehaviourScript : MonoBehaviour
 {
     Animator animator;
+    CharacterController cc;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        cc = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -17,5 +19,23 @@ public class PlayerBehaviourScript : MonoBehaviour
     {
         float dy = Input.GetAxis("Vertical");
         animator.SetFloat("Speed", dy);
+        float dx = Input.GetAxis("Horizontal");
+        animator.SetFloat("Direction", dx);
+
+        this.transform.Rotate(new Vector3(0, dx * Time.deltaTime * 100.0f, 0), Space.Self);
+        //cc.Move(this.transform.forward * dy * Time.deltaTime * 5.0f);
     }
+
+    /*
+    private void OnAnimatorMove()
+    {
+        float dy = Input.GetAxis("Vertical");
+        animator.SetFloat("Speed", dy);
+        float dx = Input.GetAxis("Horizontal");
+        animator.SetFloat("Direction", dx);
+
+        this.transform.Rotate(new Vector3(0, dx * Time.deltaTime * 75.0f, 0), Space.World);
+        cc.Move(this.transform.forward * dy * Time.deltaTime * 5.0f);
+    }
+    */
 }
